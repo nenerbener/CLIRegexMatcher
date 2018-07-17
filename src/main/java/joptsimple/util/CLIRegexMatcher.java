0d04a8017a667946e19a0871bac5bbbba90c6499
nenerbener <mceachern.marc@gmail.com>
2018-07-16 18:43:15 -0700

    initial working mvn test

---
+++ src/main/java/joptsimple/util/CLIRegexMatcher.java
@@ -0,0 +1,22 @@
+package joptsimple.util;
+
+import joptsimple.ValueConversionException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static joptsimple.util.CLIRegexMatcher.*;
+import static org.junit.Assert.*;
+import static org.junit.rules.ExpectedException.*;
+	
+public class CLIRegexMatcher {
+	/**
+	 * driver class to test RegexMatcherTest
+	 * @param args commandline arguments
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/test/java/joptsimple/util/RegexMatcherTest.java
@@ -0,0 +1,59 @@
+package joptsimple.util;
+ 
+import joptsimple.ValueConversionException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+ 
+import static joptsimple.util.RegexMatcher.*;
+import static org.junit.Assert.*;
+import org.junit.rules.ExpectedException.*;
+ 
+/**
+ * @author <a href="mailto:pholser@alumni.rice.edu">Paul Holser</a>
+ */
+public class RegexMatcherTest {
+    @Rule public final ExpectedException thrown = ExpectedException.none();
+ 
+    private RegexMatcher abc;
+ 
+    @Before
+    public void setUp() {
+        abc = new RegexMatcher( "abc", 0 );
+    }
+ 
+    @Test
+    public void shouldAttemptToMatchValueAgainstARegex() {
+        assertEquals( "abc", abc.convert( "abc" ) );
+    }
+ 
+    @Test( expected = ValueConversionException.class )
+    public void rejectsValueThatDoesNotMatchRegex() {
+        abc.convert( "abcd" );
+    }
+ 
+    @Test
+    public void raisesExceptionContainingValueAndPattern() {
+        thrown.expect( ValueConversionException.class );
+        thrown.expectMessage( "\\d+" );
+        thrown.expectMessage( "asdf" );
+ 
+        new RegexMatcher( "\\d+", 0 ).convert( "asdf" );
+    }
+ 
+    @Test
+    public void shouldOfferConvenienceMethodForCreatingMatcherWithNoFlags() {
+        assertEquals( "sourceforge.net", regex( "\\w+\\.\\w+" ).convert( "sourceforge.net" ) );
+    }
+ 
+    @Test
+    public void shouldAnswerCorrectValueType() {
+        assertEquals( String.class, abc.valueType() );
+    }
+ 
+    @Test
+    public void shouldGiveCorrectValuePattern() {
+        assertEquals( "abc", abc.valuePattern() );
+    }
+}
